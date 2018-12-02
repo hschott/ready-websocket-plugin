a28876f3af48610991563be2b0b5897cb37e7375
Holger Balow-Schott <holger.balow-schott@bitgrip.de>
2018-12-02 10:49:34 +0100

    set version to 2.0.8

--- src/main/java/com/tsystems/readyapi/plugin/websocket/PluginConfig.java
+++ src/main/java/com/tsystems/readyapi/plugin/websocket/PluginConfig.java
@@ -5,7 +5,8 @@ import com.eviware.soapui.plugins.PluginConfiguration;
 import com.eviware.soapui.support.UISupport;
 
 @PluginConfiguration(groupId = "com.tsystems.readyapi.plugin.websocket", name = "Websocket Support Plugin",
-        version = "1.0.0", autoDetect = true, description = "Adds Websocket TestSteps to SoapUI NG", infoUrl = "")
+        version = "2.0.8", autoDetect = true, description = "Adds Websocket TestSteps to SoapUI",
+        infoUrl = "https://github.com/hschott/ready-websocket-plugin/blob/master/README.md")
 public class PluginConfig extends PluginAdapter {
 
     public final static int DEFAULT_TCP_PORT = 80;
